2614b41521123b700d1610344cd68aa143e4d967
bancrafti <swishieway@gmail.com>
2025-02-19 23:09:57 +0300

    added homeview code

--- washwash/src/views/HomeView.vue
+++ washwash/src/views/HomeView.vue
@@ -1,3 +1,39 @@
 <template>
-  <h2>Home</h2>
+  <div id="navigation-bar" class="container">
+    <div id="logo">W.</div>
+    <div id="pages">
+      <router-link to="/"><div id="home" class="nav">Home</div></router-link>
+
+      <div id="cart" class="nav">Cart</div>
+      <router-link to="/about"><div id="about" class="nav">About</div></router-link>
+
+      <div id="contact" class="nav">Contact</div>
+    </div>
+  </div>
+  <div id="headline" class="container">
+    <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\wash.jpg" alt="A washing machine image" style="width: 100%;">
+    <div id="headline-text" class="text">Laundry Made Simple</div>
+    <button id="book-now">Book Now</button>
+  </div>
+  <h1>
+    Services
+  </h1>
+  <div id="services" class="services-container">
+    <div id="wash-and-fold" class="service">
+      <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\wash&fold.webp" alt="">
+      <div class="description">Wash & Fold</div>
+    </div>
+    <div id="dry-cleaning" class="service">
+      <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\dry-cleaning.jpg" alt="">
+      <div class="description">Dry Cleaning</div>
+    </div>
+    <div id="ironing-and-pressing" class="service">
+      <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\ironing&pressing.jpg" alt="">
+      <div class="description">Ironing & Pressing</div>
+    </div>
+    <div id="express-laundry" class="service">
+      <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\express-laundry.jpg" alt="">
+      <div class="description">Express Laundry</div>
+    </div>
+  </div>
 </template>
