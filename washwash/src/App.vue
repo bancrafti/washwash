2582d57ede7c48c6666e7057f0dd706170006c93
bancrafti <swishieway@gmail.com>
2025-02-19 21:12:48 +0300

    adding homepage

--- washwash/src/App.vue
+++ washwash/src/App.vue
@@ -1,85 +1,38 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="navigation-bar" class="container">
+    <div id="logo">W.</div>
+    <div id="pages">
+      <div id="home" class="nav">Home</div>
+      <div id="cart" class="nav">Cart</div>
+      <div id="about" class="nav">About</div>
+      <div id="contact" class="nav">Contact</div>
     </div>
-  </header>
+  </div>
+  <div id="headline" class="container">
+    <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\wash.jpg" alt="A washing machine image" style="width: 100%;">
+    <div id="headline-text" class="text">Laundry Made Simple</div>
+    <button id="book-now">Book Now</button>
+  </div>
+  <h1>
+    Services
+  </h1>
+  <div id="services" class="services-container">
+    <div id="wash-and-fold" class="service">
+      <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\wash&fold.webp" alt="">
+      <div class="description">Wash & Fold</div>
+    </div>
+    <div id="dry-cleaning" class="service">
+      <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\dry-cleaning.jpg" alt="">
+      <div class="description">Dry Cleaning</div>
+    </div>
+    <div id="ironing-and-pressing" class="service">
+      <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\ironing&pressing.jpg" alt="">
+      <div class="description">Ironing & Pressing</div>
+    </div>
+    <div id="express-laundry" class="service">
+      <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\express-laundry.jpg" alt="">
+      <div class="description">Express Laundry</div>
+    </div>
+  </div>
 
-  <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
