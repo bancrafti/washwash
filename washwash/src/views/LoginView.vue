feebc58724d957f1863a3c5be3c764bb13412aa7
bancrafti <swishieway@gmail.com>
2025-02-24 23:22:12 +0300

    adding login

---
+++ washwash/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Login</h1>
+  <div class="form">
+    <label for="username">Email</label>
+    <br>
+    <input type="text">
+    <br>
+    <label for="password">Password</label>
+    <br>
+    <input type="password">
+    <br><br>
+    <button type="submit">Login</button>
+  </div>
+
+</template>
