70cb0fe177c37641126715280529a8272eaa1b4f
bancrafti <swishieway@gmail.com>
2025-02-26 22:28:17 +0300

    minor changes

--- washwash/src/views/HomeView.vue
+++ washwash/src/views/HomeView.vue
@@ -5,7 +5,7 @@ import NavigationBar from '@/components/NavigationBar.vue';
 <template>
   <NavigationBar/>
   <div id="headline" class="container">
-    <img src="C:\Users\waribu\Desktop\war\washwash\washwash\src\assets\images\wash.jpg" alt="A washing machine image" style="width: 100%;">
+    <img src="@/assets/images/wash.jpg" alt="A washing machine image" style="width: 100%;">
     <div id="headline-text" class="text">Laundry Made Simple</div>
     <button id="book-now">Book Now</button>
   </div>
