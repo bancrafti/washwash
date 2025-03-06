a2696eff15dce92aecfe5800c8ee2174cb6c148d
bancrafti <swishieway@gmail.com>
2025-03-06 21:47:20 +0300

    adding signup route

--- washwash/src/router/index.js
+++ washwash/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ContactView from '@/views/ContactView.vue'
 import LoginView from '@/views/LoginView.vue'
+import SignUpView from '@/views/SignUpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path:'/login',
       name:'login',
       component:LoginView,
+    },
+    {
+      path:'/signup',
+      name:'signup',
+      component:SignUpView,
     }
   ],
 })
