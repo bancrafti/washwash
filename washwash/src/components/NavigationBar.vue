75f9203013577febf06143f7aa2401e2ab83cf91
bancrafti <swishieway@gmail.com>
2025-03-08 21:35:37 +0300

    moving css to respective files

--- washwash/src/components/NavigationBar.vue
+++ washwash/src/components/NavigationBar.vue
@@ -10,4 +10,67 @@
   </nav>
 </template>
 
-  
+<style scoped>
+  /*Navigation Bar*/
+#navigation-bar{
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  justify-content: space-between;
+  padding-right:2em;
+}
+#pages{
+  display: flex;
+  gap: 2em;
+  align-self: flex-end;
+}
+.nav{
+  font-size: 1.5em;
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+}
+/* Logo */
+#logo{
+  font-size: 2em;
+  font-family:Verdana, Geneva, Tahoma, sans-serif;
+  font-weight: bolder;
+  display: flex;
+  align-self: flex-start;
+}
+
+/* Navigation links*/
+#nav-links {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 20px;
+  background-color: #f8f9fa;
+}
+
+#logo {
+  font-size: 24px;
+  font-weight: bolder;
+  text-decoration: none;
+  color: #333;
+}
+
+#links {
+  display: flex;
+  gap: 20px;
+}
+
+#links a {
+  text-decoration: none;
+  color: #000000;
+  font-size: 18px;
+  transition: color 0.3s ease-in-out;
+}
+
+#links a:hover,
+.active-link {
+  color:palevioletred;
+  font-weight: bold;
+}
+
+</style>
--- washwash/src/views/HomeView.vue
+++ washwash/src/views/HomeView.vue
@@ -11,3 +11,73 @@ import NavigationBar from '@/components/NavigationBar.vue';
   </div>
 
 </template>
+<style scoped>
+  /* Headline */
+#headline{
+  margin-top: 1em;
+  border-radius: 0.3em;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+  position: relative;
+  text-align: center;
+  width: 100%;
+  height: 85vh;
+  object-fit: contain;
+}
+#headline-text{
+  position: absolute;
+  width: 1em;
+  top: 0.2em;
+  right: 2.99em;
+  text-align: end;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  font-size: clamp(3.8rem, 12vw, 25em);
+  font-family:Arial, Helvetica, sans-serif;
+}
+/* Button */
+#book-now{
+  position: absolute;
+  bottom: 0.2em;
+  left: 50%;
+  width: 7em;
+  height:2.2em;
+  font-size: 2em;
+  font-weight: bold;
+  transform: translate(-50%,-50%);
+  border-radius: 3em;
+  border: solid white;
+  color: white;
+  background-color: transparent;
+}
+.services-container{
+  display: grid;
+  margin-top: 2em;
+  height: 50em;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  gap: 1em;
+}
+.service{
+  position: relative;
+  background-color: rgb(0, 0, 0);
+  border-radius: 0.5em;
+  overflow: hidden;
+}
+img{
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+.description{
+  position:absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1em;
+  color: white;
+  font-weight: bolder;
+}
+</style>
