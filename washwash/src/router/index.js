c18065700941d9678093d97132d4961573ffcde8
bancrafti <swishieway@gmail.com>
2025-03-30 22:18:34 +0300

    adding services route

--- washwash/src/router/index.js
+++ washwash/src/router/index.js
@@ -4,6 +4,7 @@ import AboutView from '../views/AboutView.vue'
 import ContactView from '@/views/ContactView.vue'
 import LoginView from '@/views/LoginView.vue'
 import SignUpView from '@/views/SignUpView.vue'
+import ServicesView from '@/views/ServicesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path:'/signup',
       name:'signup',
       component:SignUpView,
+    },
+    {
+      path:'/services',
+      name:'services',
+      component:ServicesView,
     }
   ],
 })
